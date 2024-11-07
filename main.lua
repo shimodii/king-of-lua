814ac165aee0f21fb00c7ffce80788946a428c0e
shimodi <am.shahmoradi93@gmail.com>
2024-11-07 16:41:33 +0330

    make the circle moving with wasd keys

--- main.lua
+++ main.lua
@@ -5,7 +5,18 @@ function love.load()
 end
 
 function love.update(dt)
-    
+    if love.keyboard.isDown("w") then
+        player.y = player.y - 2
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - 2
+    end
+    if love.keyboard.isDown("s") then
+        player.y = player.y + 2
+    end
+    if love.keyboard.isDown("d") then
+        player.x = player.x + 2
+    end
 end
 
 function love.draw()
