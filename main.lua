2dd81e2747bf6d26ee7475312ca6b4be01a9715f
shimodi <am.shahmoradi93@gmail.com>
2024-11-07 16:43:30 +0330

    player speed seperated and added to player table

--- main.lua
+++ main.lua
@@ -2,20 +2,21 @@ function love.load()
     player = {}
     player.x = 0
     player.y = 0
+    player.speed = 2
 end
 
 function love.update(dt)
     if love.keyboard.isDown("w") then
-        player.y = player.y - 2
+        player.y = player.y - player.speed
     end
     if love.keyboard.isDown("a") then
-        player.x = player.x - 2
+        player.x = player.x - player.speed
     end
     if love.keyboard.isDown("s") then
-        player.y = player.y + 2
+        player.y = player.y + player.speed
     end
     if love.keyboard.isDown("d") then
-        player.x = player.x + 2
+        player.x = player.x + player.speed
     end
 end
 
