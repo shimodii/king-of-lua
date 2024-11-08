f7e8adc2a0745a6e4a00d192d9211b03d85fef72
shimodi <am.shahmoradi93@gmail.com>
2024-11-08 16:23:41 +0330

    problem fixed (must activate embed option during making mapgit add maps)

---
+++ maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="20" tileheight="20" tilecount="160" columns="16">
+ <image source="tileset.bmp" width="320" height="200"/>
+</tileset>
